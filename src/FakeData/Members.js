78e2cc2a9414faa88611acb9a477ce996c31bc34
AvishkaHashenu99 <global-it20186142@my.sliit.lk>
2023-11-07 04:32:15 +0530

    fb links

--- src/FakeData/Members.js
+++ src/FakeData/Members.js
@@ -5,6 +5,7 @@ export const FakeMembers = [
     status: "Supervisor",
     img: "https://i.ibb.co/5vngDxM/Ms-Jenny.jpg",
     linkedin: "https://www.linkedin.com/in/jenny-krishara-1a09a0111",
+    fb: "https://www.facebook.com/jenny.krishara",
   },
   {
     id: "002",
@@ -12,6 +13,7 @@ export const FakeMembers = [
     status: "Team Leader",
     img: "https://i.ibb.co/YTThRXN/Tinul-Piumika-v2.png",
     linkedin: "https://www.linkedin.com/in/tinul-piumika-700538218/",
+    fb: "https://www.facebook.com/tinol.tresor",
   },
   {
     id: "003",
@@ -19,6 +21,7 @@ export const FakeMembers = [
     status: "Member",
     img: "https://i.ibb.co/xDgGb3x/Isuru.jpg",
     linkedin: "https://www.linkedin.com/in/isuru-sanjeewa-28b4911b6/",
+    fb: "https://www.facebook.com/isuru.prabhath.7731",
   },
   {
     id: "004",
@@ -26,6 +29,8 @@ export const FakeMembers = [
     status: "Member",
     img: "https://i.ibb.co/f9q0MNb/avi-zoom2.png",
     linkedin: "https://www.linkedin.com/in/avishka-hashenu-58bb8b1b8/",
+    fb:
+      "https://www.facebook.com/profile.php?id=100006103368661&mibextid=ZbWKwL",
   },
   {
     id: "005",
@@ -33,5 +38,6 @@ export const FakeMembers = [
     status: "Member",
     img: "https://i.ibb.co/nn8wdW2/Chiranjaya.jpg",
     linkedin: "https://www.linkedin.com/in/chiranjaya-chandrasena-92b9a3239/",
+    fb: "https://www.facebook.com/chiranjaya.chandrasena",
   },
 ];
